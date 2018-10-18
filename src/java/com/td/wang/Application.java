40d06f7e89ed073359572e59f6b21c91833e3744
shigen.wang <shigen.wang@tendcloud.com>
2018-10-18 14:36:37 +0800

    add main

---
+++ src/java/com/td/wang/Application.java
@@ -0,0 +1,13 @@
+package com.td.wang;
+
+/**
+ * Created by @shigen.wang 2018/10/18
+ *
+ * @author shigen.wang
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
